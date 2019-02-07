94901482e932f94c95023c2e7f710fda2ad0d51f
valdislav i <vladoss_19995@mail.ru>
2019-02-07 14:29:49 +0300

    test

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,19 +5,15 @@ import AppReact from './App.jsx';
 import './index.sass';
 import './index.pug';
 import './services'
-import './products'
+import Products from './products'
 import './team'
 import './subscribe'
 import './feedback'
 
 import './test-scss.scss';
+
 export default () => {
-  console.log(tmpPug());
 
-  new Vue({
-    el: '#test-vue',
-    render: createElem => createElem(AppVue),
-  });
-
-  AppReact();
+  console.log("initProducts");
+  Products();
 };
