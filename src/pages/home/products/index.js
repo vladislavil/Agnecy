a072503e57d54231eb5a00840f829d83ec8fb2c7
valdislav i <vladoss_19995@mail.ru>
2019-02-13 15:42:16 +0300

    add plagins

--- src/pages/home/products/index.js
+++ src/pages/home/products/index.js
@@ -1,30 +1,51 @@
 import './index.sass'
+import  'blocks/tabs/index.sass'
+import 'magnific-popup/dist/jquery.magnific-popup.min.js'
+import 'magnific-popup/dist/magnific-popup.css'
 
 export default () => {
-
-  alert(1111);
-
   var AllTabs = $(".products__menu .products__item");
   var $AllTabs = $(AllTabs); // Получаем список вкладок
-  var tabContent = $(".products__tab");
-  console.log(tabContent);
+  var tabContent = $(".tab__products");
 
-  $('.products__menu').on("click", function(event){
+  tabContent.each(function (index, elem) {
+    if(index == 0)
+      $(elem).addClass("show");
+    else {
+      $(elem).addClass("hide");
+    }
+  });
+
+  $('.products__menu').on("click", function (event) {
     var $target = event.target;
-    console.log($target);
-    if($target.className == 'products__item'){
-      for(var i = 0; i < $AllTabs.length; i++){
-        if($target == $AllTabs[i]){
+    if ($target.className == 'products__item') {
+      for (var i = 0; i < $AllTabs.length; i++) {
+        if ($target == $AllTabs[i]) {
           $(tabContent[i]).addClass('show');
           $(AllTabs[i]).addClass('active');
           $(tabContent[i]).removeClass('hide');
+          $(tabContent[i]).animate({opacity: 1}, 500);
         }
         else {
           $(tabContent[i]).addClass('hide');
           $(tabContent[i]).removeClass('show');
           $($AllTabs[i]).removeClass('active');
+          $(tabContent[i]).css('opacity', 0);
         }
       }
     }
   });
+
+  $('.tab .tab__products .tab__images').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: false,
+      navigateByImgClick: false,
+      preload: [0,1]
+    }
+  });
+
 }
